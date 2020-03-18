7f68742a8a4eb876b96ef1a1ad95f7008838f127
E Versteeg <e.versteeg@solviteers.nl>
2020-03-18 12:13:24 +0100

    Try catch als er een referentie niet reageert

--- ContractApi/Services/ContractService.cs
+++ ContractApi/Services/ContractService.cs
@@ -30,31 +30,37 @@ namespace ContractApi.Services
             // Await the completion of all the running tasks. 
             var responses = await Task.WhenAll(tasks); // returns IList<IList<Contract>>>>
 
-            return responses.SelectMany(i => i).ToList(); //filter out any null values
+            return responses.Where(i => i != null).SelectMany(i => i).ToList(); //filter out any null values
         }
         public async Task<IList<Contract>> GetListOfContract(ReferencedApiModel api, string bsn)
         {
-            using var client = new HttpClient
-            {
-                BaseAddress = new Uri(api.Url)
-            };
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(api.ApiKey);
+            try { 
+                using var client = new HttpClient
+                {
+                    BaseAddress = new Uri(api.Url)
+                };
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(api.ApiKey);
 
-            //GET Method  
-            using (var response = await client.GetAsync($"api/personen/{bsn}/contracten"))
-            {
-                if (response.IsSuccessStatusCode)
+                //GET Method  
+                using (var response = await client.GetAsync($"api/personen/{bsn}/contracten"))
                 {
-                    return await JsonSerializer.DeserializeAsync<IList<Contract>>(await response.Content.ReadAsStreamAsync());
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await JsonSerializer.DeserializeAsync<IList<Contract>>(await response.Content.ReadAsStreamAsync());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Internal server Error");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Internal server Error");
-                }
+                return null;
             }
-            return null;
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
